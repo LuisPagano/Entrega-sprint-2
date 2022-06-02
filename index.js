fe2969733c0a6c62f20fda7be1b10988a524a2e3
leoD-E1 <leodangeli11@gmail.com>
2022-06-02 20:38:05 -0300

    [finish] function for catch data from the form

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let nombre="Nombre";
-let monto=5;
-const listaNombreYmonto = document.querySelector('#nombreYmonto');
-function ejecutar(){
-    mostrarNombreYmonto(nombre,monto);
-}
-function mostrarNombreYmonto (_nombre,_monto) {
-    listaNombreYmonto.innerHTML += `<li class="list-group-item">${_nombre}: $${_monto}</li>`;
+const inputName = document.getElementById("name");
+const inputCost = document.getElementById("cost");
+const submitButton = document.getElementById("submit");
+const listaNombreYmonto = document.querySelector("#nombreYmonto");
+
+let peopleName = "";
+let cost = 0;
+
+inputName.addEventListener("change", (evt) => catchName(evt));
+inputCost.addEventListener("change", (evt) => catchCost(evt));
+
+const catchName = (evt) => (peopleName = evt.target.value);
+const catchCost = (evt) => (cost = evt.target.value);
+
+const ejecutar = () => {
+  mostrarNombreYmonto(peopleName, cost);
+  inputName.value = "";
+  inputCost.value;
+};
+
+function mostrarNombreYmonto(_nombre, _monto) {
+  listaNombreYmonto.innerHTML += `<li class="list-group-item">${_nombre}: $${_monto}</li>`;
 }
