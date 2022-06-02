e8ea1310a97b6695527c7ff80aab0f9ca65e35c3
LautyT02 <lautyt02@gmail.com>
2022-06-01 22:02:05 -0300

    Primera versión de impresión nombre y monto
    El resto tiene que agregar su parte para poder realizar modificaciones

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let nombre="Nombre";
 let monto=5;
-const listaNombreYmonto = document.querySelector('nombreYmonto');
-function mostrarNombreYmonto () {
-
+const listaNombreYmonto = document.querySelector('#nombreYmonto');
+function ejecutar(){
+    mostrarNombreYmonto(nombre,monto);
+}
+function mostrarNombreYmonto (_nombre,_monto) {
+    listaNombreYmonto.innerHTML += `<li class="list-group-item">${_nombre}: $${_monto}</li>`;
 }
