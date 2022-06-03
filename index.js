d9a7998de9d264abeb8741cc273a7adcf76752b8
Luis <sergioluispagano@gmail.com>
2022-06-03 06:15:32 -0300

    Check arreglado, código hecho más legible, planes a futuro

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-const inputName = document.getElementById("name");
-const inputCost = document.getElementById("cost");
-const submitButton = document.getElementById("submit");
-const listaNombreYmonto = document.querySelector("#nombreYmonto");
+let nombre = "";
+let monto = 1;
+let todosLosGastos = [];
+//variables
 
-let peopleName = "";
-let cost = 0;
+var inputNombre = document.getElementById("pedirNombre");
+var inputGasto = document.getElementById("pedirMonto");
+var listaNombreYmonto = document.querySelector("#nombreYmonto");
+var submitButton = document.getElementById("submit");
+var totalParaMostrar = document.getElementById("total");
+//nuevas variables para manejar lo que venga del input
 
-inputName.addEventListener("change", (evt) => catchName(evt));
-inputCost.addEventListener("change", (evt) => catchCost(evt));
+inputNombre.addEventListener("change", (evt) => catchName(evt));
+inputGasto.addEventListener("change", (evt) => catchCost(evt));
 
-const catchName = (evt) => (peopleName = evt.target.value);
-const catchCost = (evt) => (cost = evt.target.value);
-
-const ejecutar = () => {
-  mostrarNombreYmonto(peopleName, cost);
-  inputName.value = "";
-  inputCost.value;
-};
+const catchName = (evt) => (nombre = evt.target.value);
+const catchCost = (evt) => (monto = evt.target.value);
+//hace que nombre y  monto tenga los valores que les dan?
 
 function mostrarNombreYmonto(_nombre, _monto) {
   listaNombreYmonto.innerHTML += `<li class="list-group-item">${_nombre}: $${_monto}</li>`;
+}
+//añade el nombre y el gasto al html
+
+function check() {
+  return inputGasto.validity.rangeUnderflow
+}
+//checkea si el monto está por debajo de 1
+
+// function calcTotal(array){
+//   let suma = 0
+//   for (let elemento of array){
+//       suma += elemento;
+//   }
+//   return suma;
+// }
+
+// function calcPromedioTotal(array){
+//   let promedio = 0
+//   promedio = calcTotal(array)/array.length
+//   return promedio
+// }
+
+// function mostrarTotal() {
+//   totalParaMostrar.innerHTML += `<p>$${calcTotal(todosLosGastos)}</p>`;
+// }
+
+function enviar(){
+  if (check()){
+     alert("Ingrese un monto válido");
+     //alerta cuando debajo de 1
+  } else {
+    mostrarNombreYmonto(nombre, monto);
+    //poner los nombres y montos en pantalla
+    inputNombre.value = "";
+    inputGasto.value = 0;
+    monto = 0
+  }
 }
